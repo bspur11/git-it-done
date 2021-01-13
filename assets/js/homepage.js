85ac5f42605dca97f8e3689caf9aff92811bb39d
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-01-12 22:00:38 -0500

    start form input

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,8 @@
 var getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
-};
-
-// getUserRepos();
+  fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+}
+getUserRepos();
