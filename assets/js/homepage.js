fe48d13ddfd04cb046788ee4e2f93b746dfce807
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-01-16 21:36:28 -0500

    query paramiter

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -58,10 +58,20 @@ var displayRepos = function (repos, searchTerm) {
     //  formate repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-    //  create a container for each repo
-    var repoEl = document.createElement("div");
+    //  create a container for each repo643
+
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html");
+
+    // create a link for each repo 643
+
+    var repoEl = document.createElement("a");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+
     // create a span element to hold repository name
+
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
     //  append to container
